ac6981eb535c2b6a957509ef26a7357ca29234d1
tom reifenberg <reifenberg.57@gmail.com>
2020-11-19 15:37:15 -0600

    Handling Dynamic Content

--- src/App.js
+++ src/App.js
@@ -41,13 +41,33 @@ togglePersonsHandler = () => {
 }
 
   render() {
-const style = {
-  backgroundColor: 'white',
-  font: 'inherit',
-  border: '1px solid blue',
-  padding: '8px',
-  cursor:'pointer'
-};
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor:'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}  />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Tom!')}
+          changed={this.nameChangedHandler}>My Hobbies: Partying</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
+      </div>
+      )
+    }
 
     return (
         <div className="App">
@@ -56,22 +76,8 @@ const style = {
           <button 
             style={style}
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          {
-            this.state.showPersons ? 
-              <div>
-              <Person 
-                name={this.state.persons[0].name} 
-                age={this.state.persons[0].age}  />
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Tom!')}
-                changed={this.nameChangedHandler}>My Hobbies: Partying</Person>
-              <Person 
-                name={this.state.persons[2].name} 
-                age={this.state.persons[2].age}/>
-            </div> : null
-          }
+        
+        {persons}
 
       </div>
     );
